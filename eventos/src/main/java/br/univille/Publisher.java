ea052329d3c882ae9ee0e956010ed9202f826047
Thales Pedri <thalesnatan03@gmail.com>
2025-09-15 22:56:17 +0000

    servicebus

--- eventos/src/main/java/br/univille/Publisher.java
+++ eventos/src/main/java/br/univille/Publisher.java
@@ -1,16 +1,35 @@
 package br.univille;
 
+import com.azure.core.amqp.AmqpTransportType;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusMessage;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
 public class Publisher {
     public static void main(String[] args) {
         var topicName = "topic-das1-b";
-        var servidor = "https://sbdas12025a.servicebus.windows.net";
+        var servidor = "sbdas12025a.servicebus.windows.net";
+        
+        //String javaHome = System.getenv("JAVA_HOME");
 
-        DefaultAzureCredential credential =
+        DefaultAzureCredential credential = 
             new DefaultAzureCredentialBuilder().build();
 
-    }
+        
+        ServiceBusSenderClient senderClient =
+            new ServiceBusClientBuilder()
+                .fullyQualifiedNamespace(servidor)
+                .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
+                .credential(credential)
+                .sender()
+                .topicName(topicName)
+                .buildClient();
 
+        senderClient.sendMessage(
+            new ServiceBusMessage("Thales: ...")
+        );
+        
+    }
 }
